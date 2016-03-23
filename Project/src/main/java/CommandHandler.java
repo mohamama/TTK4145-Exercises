77f5fcfff62526a8d41a689e20f862433e63311b
Torbjørn Viem Ness <tbness@gmail.com>
2016-03-23 16:53:23 +0100

    Formulated an outline for how to handle and dispatch commands in the system

---
+++ Project/src/main/java/CommandHandler.java
@@ -0,0 +1,22 @@
+/**
+ * Created by tovine on 3/23/16.
+ */
+public class CommandHandler {
+    /*
+     * Algorithm outline:
+     * ------------------
+     *  1. When the user presses a 'call' button, a command is broadcast to the elevator cluster
+     *  2. The elevators then (upon receiving the message) calculate a "cost" for them to respond, based on the following:
+     *    - 1 point for every floor between the caller and the elevator's current position
+     *    - If the elevator is moving in the direction the caller wants to go, add 1 extra point to allow an idle elevator to take priority (TODO: or not?)
+     *    - If the elevator is moving in the other direction it is assumed "busy"
+     *    - Also add a random delay (< 1 point) to avoid two elevators responding at exactly the same time
+     *    TODO: more conditions?
+     *  3. After estimating the cost, a delay period is calculated based on the number of points (multiplying by a number of milliseconds - TODO: tune it)
+     *    3.1: The delay period starts running only when the elevator is idle (or moving in the right direction for the job)
+     *  4. Once the delay period expires, the elevator takes the job (light on and start moving) and broadcasts a message notifying the others that it's been handled
+     *    4.1: All other elevators abort the job upon receiving this broadcast (and store the ID of the elevator handling it)
+     *    4.2: If that elevator goes offline, the job is redistributed among the remaining ones in the same manner as before
+     *  5. Finally once the job is done, the elevator who completed it broadcasts to the others that the order is processed and can be safely deleted from the system
+     */
+}
